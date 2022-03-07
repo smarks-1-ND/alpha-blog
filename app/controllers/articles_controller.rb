ebcc8a878e44166a4a0ed483447a908231cf7d4b
Steve Marks <smarks1@nd.edu>
2022-03-07 16:18:06 -0500

    DRY edits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy] 
 
     def show
         @article = Article.find(params[:id])
@@ -18,7 +19,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         #render plain: @article.inspect
         if @article.save 
             flash[:notice] = "Article was created successfully"
@@ -30,7 +31,7 @@ class ArticlesController < ApplicationController
 
     def update
         @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully"
             redirect_to @article
         else
@@ -48,6 +49,10 @@ class ArticlesController < ApplicationController
 
     def set_article
         @article = Article.find(params[:id])
+    end
+
+    def article_params 
+        params.require(:article).permit(:title, :description)
     end
 
 
