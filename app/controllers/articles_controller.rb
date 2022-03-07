16eef87217b50f7d8f98af86456fd51f44f8bf05
Steve Marks <smarks1@nd.edu>
2022-03-07 14:23:31 -0500

    updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,35 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new 
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article.inspect
+        if @article.save 
+            flash[:notice] = "Article was created successfully"
+            redirect_to article_path(@article)
+        else 
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end 
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles # Now that we have exposed all routes, don't need specific# , only: [:show, :index, :new, :create, :edit, :update]
 end
