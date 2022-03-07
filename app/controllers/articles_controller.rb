72e355372505a161838bf0b7dfea00cddfbb834f
Steve Marks <smarks1@nd.edu>
2022-03-07 15:47:35 -0500

    set_article def

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,5 +38,18 @@ class ArticlesController < ApplicationController
         end
     end 
 
+    def destroy 
+        @article = Article.find(params[:id])
+        @article.destroy 
+        redirect_to articles_path
+    end
+
+    private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+
 end
 
